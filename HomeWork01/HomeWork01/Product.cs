67d003752af6deb231411b591a8a5684ccf7674c
Mikhail Efremov <crv.show@gmail.com>
2017-11-13 13:18:59 +0200

    first upload

---
+++ HomeWork01/HomeWork01/Product.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* TASK:
+ * создать класс у которого будет три филда два конструктора три проперти и будет оверрайд тустринг
+ * создать экземпляр и вывести значения на консоль
+ * 186-191
+ * 215-220
+ * 222-226
+ * 263-285
+
+*/
+
+
+namespace HomeWork01
+{
+    class Product
+    {
+        private uint _id;
+        private double _price;
+        private string _name;
+
+        public uint Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+        
+
+        public double Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public Product()
+        {
+            _id = 0;
+            _price = 0;
+            _name = "Unknown";
+        }
+
+        public Product(uint id)
+        {
+            _id = id;
+            _price = 0;
+            _name = "Unknown";
+        }
+
+        public Product(uint id, float price)
+        {
+            _id = id;
+            _price = price;
+            _name = "Unknown";
+        }
+
+        public Product(uint id, double price, string name)
+        {
+            _id = id;
+            _price = price;
+            _name = name;
+        }
+
+        public override string ToString()
+        {
+            return _name;
+        }
+    }
+}
---
+++ HomeWork01/HomeWork01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product = new Product(1, 100.5, "Iphone X");
+            Console.WriteLine(product);
+            product.Id = 10;
+            product.Name = "Samsung";
+            Console.WriteLine(product);
+            Console.WriteLine($"Id: {product.Id}, Name: {product.Name}, Price: {product.Price}");
+        }
+    }
+}
